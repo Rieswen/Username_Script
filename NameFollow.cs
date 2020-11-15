e44959694c94e1a77902797d457e3633a7e9ad82
Rares <67231221+Rieswen@users.noreply.github.com>
2020-11-15 16:02:13 +0200

    Add files via upload

---
+++ NameFollow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+
+public class NameFollow : MonoBehaviour
+{
+    public TMP_InputField username;
+    public TextMeshPro ftext;
+    public GameObject noUsernameCheck;
+    public GameObject SetUsernamePanel;
+    public bool isClicked = false;
+    void Start()
+    {
+    }
+
+    public void OnButtonClicked()
+    {
+       if(username.text == ""){
+            isClicked = false;
+            print("Nope");
+            SetUsernamePanel.SetActive(true);
+            noUsernameCheck.SetActive(true);
+        } else if (username.text != ""){
+            isClicked = true;
+            ftext.text = username.text;
+            print("yup");
+            SetUsernamePanel.SetActive(false);
+            noUsernameCheck.SetActive(true);
+        }
+    }
+}
